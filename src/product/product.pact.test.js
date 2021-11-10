74da0490954f09c85bc3f324be1824d17ad551e5
Yuriy Sorokin <y.sorokin@napoleongames.be>
2021-11-10 16:26:00 +0600

    Enable Pact WIP pacts feature
    Signed-off-by: Yuriy Sorokin <y.sorokin@napoleongames.be>

--- src/product/product.pact.test.js
+++ src/product/product.pact.test.js
@@ -34,7 +34,8 @@ describe("Pact Verification", () => {
       consumerVersionSelectors: [{ tag: 'master', latest: true }, { deployed: true } ], // the new way of specifying which pacts to verify
       pactBrokerUrl: process.env.PACT_BROKER_BASE_URL,
       enablePending: true,
-      includeWipPactsSince: undefined
+      // includeWipPactsSince: undefined
+      includeWipPactsSince: "2020-01-01"
     }
 
     const stateHandlers = {
